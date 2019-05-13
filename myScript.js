c3ee7a8df47d05a86612aac3045e853692cde9d0
Hemanth Mannava <m.hemanth83@gmail.com>
2019-05-13 14:07:54 +0530

    added assesment files

---
+++ myScript.js
@@ -0,0 +1,7 @@
+
+function validate(form){
+   if(form.Username.value=="User" && form.pswd.value=="password"){
+       alert("Login Successfully!");
+   }else
+       alert("Error Username or Password");
+}
